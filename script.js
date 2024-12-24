dccd5ea08e528fc41b29516756142d522ac74df2
alimakbar31 <61396694+alimakbar31@users.noreply.github.com>
2024-12-24 16:15:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // Initialize Telegram Web App
-const tg = window.Telegram.WebApp;
+// const tg = window.Telegram.WebApp;
 
-// Handling click on the 'Join Airdrop' button
-document.getElementById('startAirdrop').addEventListener('click', () => {
-    // Send data to Telegram bot (could be used for backend processing)
-    tg.sendData("airdrop_joined");
+// // Handling click on the 'Join Airdrop' button
+// document.getElementById('startAirdrop').addEventListener('click', () => {
+//     // Send data to Telegram bot (could be used for backend processing)
+//     tg.sendData("airdrop_joined");
 
-    // Close Web App and return to Telegram
-    tg.close();
+//     // Close Web App and return to Telegram
+//     tg.close();
+// });
+
+// Select all menu items
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Add click event to each menu item
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        // Remove active class from all menu items
+        menuItems.forEach(menu => menu.classList.remove('active'));
+
+        // Add active class to clicked menu item
+        item.classList.add('active');
+    });
 });
